cd70b589413ef933f8f5e6b6932774473f4f51a0
dmitr <dmitriy.porter@mail.ru>
2023-04-17 20:37:52 +0300

    test

--- src/main/java/ru/mephi/tasuku/task/TaskRepository.java
+++ src/main/java/ru/mephi/tasuku/task/TaskRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.mephi.tasuku.task.model.TaskModel;
 
 public interface TaskRepository extends JpaRepository<TaskModel, Long> {
+
+    ////
 }
