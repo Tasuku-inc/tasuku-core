727089841f7ddce700ee3bf69a80abf19005a606
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2023-04-30 08:05:26 +0300

    little fix

--- src/main/java/ru/mephi/tasuku/task/controller/dto/TaskUpdateRequest.java
+++ src/main/java/ru/mephi/tasuku/task/controller/dto/TaskUpdateRequest.java
@@ -1,12 +1,10 @@
 package ru.mephi.tasuku.task.controller.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import ru.mephi.tasuku.task.repository.model.TaskStatus;
 
 @Data
 public class TaskUpdateRequest {
-	@NotBlank(message = "Task id can't be blank or empty")
 	private long id;
 	private String name;
 	private Long assigneeId;
